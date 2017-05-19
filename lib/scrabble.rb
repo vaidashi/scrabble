87fefbcd1c1d98c071b7cd2e19360fd21dbd99e7
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-05-19 12:04:24 -0600

    scrabble

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,11 +1,34 @@
 class Scrabble
 
-  def score(word)
-    1
+attr_accessor :values, :point_values
+
+
+  def initialize
+
+    @values = values
+    @point_values = point_values
+
   end
 
+  def score(word)
+    letter_score = word.chars.map do |letter|
+      @values.values[letter]
+    end
+    letter_score
+  end
+
+  # var = input.chars.map do |letter|
+  #   @braille.braille[letter]
+  # end
+  # var
+
+
+
+
   def point_values
-    {
+
+    @values = {
+
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
@@ -14,5 +37,10 @@ class Scrabble
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
       "Y"=>4, "Z"=>10
     }
+
   end
+
+
+
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,15 @@ require 'minitest/emoji'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+
   def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+
+
+    assert_equal 1, Scrabble.new.score("A")
+    assert_equal 4, Scrabble.new.score("F")
   end
+
+
+
+
 end
